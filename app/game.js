fe05be4950cbf84b4f49dd7938f44e3c6449a68c
larix45 <milarczek2004@gmail.com>
2020-11-25 18:16:58 +0100

    aktualizacja game.js (dodanie nurka itd.)

--- app/game.js
+++ app/game.js
@@ -1,7 +1,15 @@
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: {
+                y: -180
+            }
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -11,7 +19,7 @@ var config = {
 
 var game = new Phaser.Game(config);
 var nurek;
-
+var atmosfera;
 function preload ()
 {
     this.load.image("nurek","./assets/nurek.png");
@@ -19,9 +27,19 @@ function preload ()
 
 function create ()
 {
+    var woda  = this.add.rectangle(0, 101, 800, 600, 0x2020c0);
+    woda.setOrigin(0,0);
+    atmosfera = this.add.rectangle(0,     0, 800, 100, 0xe0e0f0);
+    atmosfera.setOrigin(0,0);
+    this.physics.add.existing(atmosfera);
+    atmosfera.body.setCollideWorldBounds();
+
     nurek = this.physics.add.sprite(100,100,"nurek");
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
+    nurek.setBounce(0.2);
+    nurek.setCollideWorldBounds(true);
+    this.physics.add.collider(nurek, atmosfera);
+    
+
 
 }
 
@@ -36,4 +54,9 @@ function update ()
     {
         nurek.setVelocityX(-10);
     }
+    if(cursors.down.isDown)
+    {
+        nurek.setVelocityY(100);
+    }
+    
 }
