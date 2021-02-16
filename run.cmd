be23be29c27131e25fe40a74a6d03238533b0d29
Oskar <oskarek.i.tomirek@gmail.com>
2021-02-16 15:07:44 +0100

    Compilation bugs fixes

--- run.cmd
+++ run.cmd
@@ -1,5 +1,10 @@
 @echo off
+
 cd frontend
+
+if not exist node_modules (
+    call yarn
+)
 if %1 == dev (
     call yarn start
     rem goto :spring
@@ -12,12 +17,18 @@ if %1 == dev (
     exit
 )
 
+
 cd ..
+
+if not exist server/public (
+    mkdir server/public
+)
+
 cd game
 
+rem TODO: try to disable GUI at godot export
 godot --no-window --path ./project.godot --export HTML5 ../server/public/game.html
 
-rem :spring
 cd ..
 cd server
 mvnw spring-boot:run
