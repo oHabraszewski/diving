18080ea5857a31c011dddeb8fd09f25e7b0d617f
Akr0nys <55049058+larix45@users.noreply.github.com>
2021-02-21 12:43:25 +0100

    Added starting place
    RN it is ugly, but it will be retouched in the future.

--- game/main.gd
+++ game/main.gd
@@ -6,20 +6,17 @@ extends Node2D
 # var b = "text"
 var terrain = preload("res://elements/terrain/terrain.tscn")
 onready var active_terrain = $Game/terrain
-
-
 var net_generation_seed = "some jubrish"
 var actual_seed = hash(net_generation_seed)
+var terrain_chunks = []
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	active_terrain.generate_terrain_v5(actual_seed, 200, 200, 20, 30, Vector2(100,100))
+	terrain_chunks.append(active_terrain)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
-	
-	
 	$Interface/Control/Label.text = "Player position: " + String($Game/RigidBody2D.position) #DEV
 	pass
 
@@ -40,7 +37,10 @@ func _on_terrain_entered_body():
 	actual_seed = hash(actual_seed+1)
 	active_terrain.generate_terrain_v5(actual_seed, 200, 200, 20, 30, Vector2(100,100))
 	print("NEW SEED: ", actual_seed)
-	
+	terrain_chunks.append(active_terrain)
+	while terrain_chunks.size() >= 4:#Deleating not needed anymore chunks of the world
+		terrain_chunks[0].queue_free()
+		terrain_chunks.remove(0)
 	
 	
 	## Problematic SEED = 1454487252
