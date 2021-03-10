d89687fcf4079f22eb1f0a13789efc496fff6c39
Oskar <oskarek.i.tomirek@gmail.com>
2021-03-10 19:00:57 +0100

    New index aka login page look, validation not working yet

---
+++ frontend/src/public/Vue/Link.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <a class="link" v-on:click="changeDir">{{text}}</a>
+    </div>
+</template>
+<script>
+    export default {
+        props: ['text','destination'],
+        methods: {
+            changeDir(){
+                if(this.destination != undefined){
+                    location.href = this.destination
+                }
+            }
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    // .buttonContainer{
+    //     width: 350px;
+    //     height: 56px;
+    // }
+    // .link {
+    //     position: absolute;
+    //     left: 175px;
+    //     transform: translate(-50%, 0%);
+    //     background: rgba($color: #367169, $alpha: 0.6);
+    //     margin: 2px;
+    //     border-radius: 12px;
+    //     border: solid #103131 3px;
+    //     width: 350px;
+    //     height: 56px;
+    //     padding: 0 3px 0 3px;
+    // }
+    .link:hover {
+        cursor: pointer;
+    }
+    // .button:focus{
+    //     animation: squeeze 0.4s ease-in-out;
+    //     animation-iteration-count: 1;
+        
+    // } 
+</style>
--- frontend/src/public/Vue/Login.vue
+++ frontend/src/public/Vue/Login.vue
@@ -1,13 +1,20 @@
 <template>
     <div class="login">
-        <h1>Diving</h1>
-        <Input title="Type world name" placeholder="World name" :default=world_id></Input>
-        <Button text="Play" destination="/game"></Button>
+        <div class="newlogin">
+            <h1>Diving</h1>
+            <form>
+                <Input title="Type your username" placeholder="Username"></Input>
+                <Input title="Type your password" placeholder="Password" type="password"></Input>
+                <Button text="Play" destination="/game"></Button>
+            </form>
+            <Link text="Don't have an account?" destination="/register"></Link>
+        </div>
     </div>
 </template>
 <script>
     import Button from './Button.vue'
     import Input from './Input.vue'
+    import Link from './Link.vue'
 
     export default {
         props: ['nickname'],
@@ -18,7 +25,8 @@
         },
         components: {
             Button,
-            Input
+            Input,
+            Link
         }
     }
 </script>
