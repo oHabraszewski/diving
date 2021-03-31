09f1052f728f60dd6ab3ab40d32af9bf2fefb37f
Oskar <oskarek.i.tomirek@gmail.com>
2021-03-31 15:14:43 +0200

    Technical and credits template injection

---
+++ frontend/src/technical.js
@@ -0,0 +1,12 @@
+import "./public/scss/main.scss"
+
+import * as Vue from "vue"
+import Login from "./public/vue/Login.vue"
+
+const App = {
+    components: {
+        Login
+    }
+}
+  
+Vue.createApp(App).mount('#app')
