91ec7b5848aaa1aed31bae023046d1a7deb7c8f6
Akr0nys <55049058+larix45@users.noreply.github.com>
2021-03-31 16:01:17 +0200

    generation update

--- game/elements/terrainv2/Terrain.gd
+++ game/elements/terrainv2/Terrain.gd
@@ -4,22 +4,49 @@ extends Node2D
 # var a = 2
 # var b = "text"
 var terrain_chunk = preload("res://elements/terrainv2/TerrainChunk.tscn")
-var index = 0 #chunk na ktorym jest gracz aktualnie
-var current_instance
+var chunks = {}
+var current_chunks = {}
 
-
-# Called when the node enters the scene tree for the first time.
+func load_chunks():
+	pass
+func save_chunks():
+	pass
+	
+func add_chunk(current_chunks: Dictionary, front: bool, generation_seed: int): #funkcja dodaje chunk z podanym seedem
+	print("DBG: ", current_chunks.keys().max()+1)
+	var incrementar 
+	var instance = terrain_chunk.instance()
+	if front:
+		instance.position = Vector2(current_chunks[current_chunks.keys().max()].position.x + current_chunks[current_chunks.keys().max()].segment_size.x/2 + current_chunks[current_chunks.keys().max()].segment_size.x * current_chunks[0].segment_count_x, current_chunks[current_chunks.keys().max()].position.y)
+		incrementar = current_chunks.keys().max()+1
+	else:
+		instance.position = Vector2(current_chunks[current_chunks.keys().min()].position.x - current_chunks[current_chunks.keys().min()].segment_size.x/2 - current_chunks[current_chunks.keys().min()].segment_size.x * current_chunks[current_chunks.keys().min()].segment_count_x, current_chunks[0].position.y)
+		incrementar = current_chunks.keys().min()-1
+	current_chunks[incrementar] = instance
+	current_chunks[incrementar].generate()
+	self.add_child(current_chunks[incrementar])
+	
+	pass
+	
+	
 func _ready():
-	current_instance = terrain_chunk.instance()
-	current_instance.generate()
-	self.add_child(current_instance)
+#	current_instance = terrain_chunk.instance()
+#	current_instance.generate()
+#	self.add_child(current_instance)
+	
+#	[-2][-1][0][1][2][3]
+#	[  ][  ][ ][ ][ ][ ]
+	
+	#if new game
+	current_chunks[0] = terrain_chunk.instance()
+	current_chunks[0].generate()
+	self.add_child(current_chunks[0])
+	self.position.y = self.position.y + current_chunks[0].segment_size.y * current_chunks[0].segment_count_y #ustawianie wysokości
+	add_chunk(current_chunks, true, 1)
+	add_chunk(current_chunks, false, 1)
 	
 	
-	
-	
-	#ustawianie wysokości
-	self.position.y = self.position.y + current_instance.segment_size.y * current_instance.segment_count_y
-	pass # Replace with function body.
+	pass 
 
 
 
