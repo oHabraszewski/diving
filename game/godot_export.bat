71db73891902b88d88353fab797cb7f9e610e1ec
Oskar <oskarek.i.tomirek@gmail.com>
2021-09-28 21:13:14 +0200

    File structure changes before Heroku integration

--- game/godot_export.bat
+++ game/godot_export.bat
@@ -1,5 +1,5 @@
 @echo off
 :start
-..\..\godot --no-window -w --path ./project.godot --export HTML5 ../server/public/game.html
+..\..\godot --no-window -w --path ./project.godot --export HTML5 ../server/src/main/resources/public/game.html
 pause
 goto :start
