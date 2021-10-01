8427c9892a198d60b31ab17e9326bfe02df2a367
Oskar <oskarek.i.tomirek@gmail.com>
2021-10-01 22:25:15 +0200

    Server source cleanup

--- server/src/main/java/tk/chaber/sfn2021rest/db/PlayerDatasRepo.java
+++
@@ -1,7 +0,0 @@
-package tk.chaber.sfn2021rest.db;
-
-import org.springframework.data.repository.CrudRepository;
-import tk.chaber.sfn2021rest.db.entities.PlayerData;
-
-public interface PlayerDatasRepo extends CrudRepository<PlayerData, Integer> {
-}
--- server/src/main/java/tk/chaber/sfn2021rest/db/RoomsRepo.java
+++
@@ -1,7 +0,0 @@
-package tk.chaber.sfn2021rest.db;
-
-import org.springframework.data.repository.CrudRepository;
-import tk.chaber.sfn2021rest.db.entities.Room;
-
-public interface RoomsRepo extends CrudRepository<Room, Integer> {
-}
