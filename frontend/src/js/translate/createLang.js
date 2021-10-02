a06a4fba80873c10f2177a45535a0e14d6363bbe
Oskar <oskarek.i.tomirek@gmail.com>
2021-10-03 00:19:02 +0200

    Patch of critical error related to translation system

--- frontend/src/js/translate/createLang.js
+++ frontend/src/js/translate/createLang.js
@@ -2,7 +2,12 @@ import polish from "../../../assets/json/translate/polish.json"
 import english from "../../../assets/json/translate/english.json"
 
 function createLang(page){
+    if(localStorage.getItem("lang") == undefined){
+        localStorage.setItem("lang", "EN")
+    }
+
     const lang = localStorage.getItem("lang")
+
     let langObj
     switch(lang){
         case "EN":
