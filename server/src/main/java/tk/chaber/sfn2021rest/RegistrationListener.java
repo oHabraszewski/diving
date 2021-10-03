7a73eacdbcbfdb603318547f56152bbc831b5048
Oskar <oskarek.i.tomirek@gmail.com>
2021-10-03 13:19:57 +0200

    RegistrationListener basic code

---
+++ server/src/main/java/tk/chaber/sfn2021rest/RegistrationListener.java
@@ -0,0 +1,47 @@
+package tk.chaber.sfn2021rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.MessageSource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+import tk.chaber.sfn2021rest.db.UserService;
+import tk.chaber.sfn2021rest.db.entities.User;
+
+import java.util.Locale;
+import java.util.UUID;
+
+@Component
+public class RegistrationListener implements ApplicationListener<OnRegistrationCompleteEvent> {
+    @Autowired
+    private UserService service;
+
+    @Autowired
+    private MessageSource messages;
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Override
+    public void onApplicationEvent(OnRegistrationCompleteEvent onRegistrationCompleteEvent) {
+
+    }
+
+    private void sendConfirmRegistration(OnRegistrationCompleteEvent event){
+        User user = event.getUser();
+        String token = UUID.randomUUID().toString();
+        service.createVerificationToken(user, token);
+
+        String recipientEmail = user.getEmail();
+        String subject = "Confirm your registration to the Diving game";
+        String confirmationUrl = event.getAppUrl() + "/registrationConfirm?token=" + token;
+        String message = messages.getMessage("message.regSucc", null, Locale.US);
+
+        SimpleMailMessage email = new SimpleMailMessage();
+        email.setTo(recipientEmail);
+        email.setSubject(subject);
+        email.setText(message + "\r\n" + confirmationUrl);
+        mailSender.send(email);
+    }
+}
