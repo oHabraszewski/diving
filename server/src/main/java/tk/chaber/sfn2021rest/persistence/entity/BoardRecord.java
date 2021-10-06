6cb52ba37e4227d18049a0c4f0d576e82bfb7a5a
Oskar <oskarek.i.tomirek@gmail.com>
2021-10-06 16:45:12 +0200

    Time constructor and reader in BoardRecord

--- server/src/main/java/tk/chaber/sfn2021rest/persistence/entity/BoardRecord.java
+++ server/src/main/java/tk/chaber/sfn2021rest/persistence/entity/BoardRecord.java
@@ -1,38 +1,58 @@
 package tk.chaber.sfn2021rest.persistence.entity;
 
 import javax.persistence.*;
-import java.util.Date;
 
 @Entity
 public class BoardRecord {
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    @Column(name="id", nullable = false, updatable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private Long time;
 
-    private Integer score;
+    private Long score;
 
     @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
     @JoinColumn(nullable = false, name="user_id")
     private User user;
 
+    private Long calculateTime(String timeInMMSS){
+        String[] times = timeInMMSS.split(":");
+        long minutes = Integer.parseInt(times[0]);
+        long seconds = Integer.parseInt(times[1]);
 
+        return (60 * minutes + seconds);
+    }
+
+    public BoardRecord() {
+        super();
+    }
+
+    public BoardRecord(User user, Long score, String timeString){
+        this.user = user;
+        this.score = score;
+        this.time = calculateTime(timeString);
+    }
 
     public Long getTime() {
         return time;
+    }
+
+    public String getTimeInString(){
+        long minutes = time/60;
+        long seconds = time%60;
+        return minutes + ":" + seconds;
     }
 
     public void setTime(Long time) {
         this.time = time;
     }
 
-    public Integer getScore() {
+    public Long getScore() {
         return score;
     }
 
-    public void setScore(Integer score) {
+    public void setScore(Long score) {
         this.score = score;
     }
 
