8ee8be8a9689f776ce8ca56b2398d5e2745ce732
Oskar <oskarek.i.tomirek@gmail.com>
2021-10-07 21:54:06 +0200

    Change in sorting algorithm.

--- server/src/main/java/tk/chaber/sfn2021rest/persistence/repository/BoardRepo.java
+++ server/src/main/java/tk/chaber/sfn2021rest/persistence/repository/BoardRepo.java
@@ -1,5 +1,6 @@
 package tk.chaber.sfn2021rest.persistence.repository;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 import tk.chaber.sfn2021rest.persistence.entity.BoardRecord;
 import tk.chaber.sfn2021rest.persistence.entity.User;
@@ -7,9 +8,7 @@ import tk.chaber.sfn2021rest.persistence.entity.User;
 import java.util.List;
 
 public interface BoardRepo extends CrudRepository<BoardRecord, Long> {
-    List<BoardRecord> findByOrderByScoreDesc();
-
-    List<BoardRecord> findByOrderByTimeAsc();
+    List<BoardRecord> findAll(Sort sort);
 
     BoardRecord findByUser(User user);
 }
